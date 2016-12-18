c0319393d8f283907a53195e3dbb93d64193d2af
Erik Godard <godarderik@gmail.com>
2016-12-18 02:10:21 -0500

    refactor handout code, start groundwork for particles

---
+++ Particle.h
@@ -0,0 +1,17 @@
+//
+//  Particle.h
+//  CS171FinalProject
+//
+//  Created by Erik Godard on 12/17/16.
+//  Copyright © 2016 Erik Godard & Jessica Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Particle : NSObject
+
+@property (nonatomic, assign) float position;
+@property (nonatomic, strong) NSArray<NSNumber>;
+
+
+@end
---
+++ Particle.m
@@ -0,0 +1,13 @@
+//
+//  Particle.m
+//  CS171FinalProject
+//
+//  Created by Erik Godard on 12/17/16.
+//  Copyright © 2016 Erik Godard & Jessica Wang. All rights reserved.
+//
+
+#import "Particle.h"
+
+@implementation Particle
+
+@end
